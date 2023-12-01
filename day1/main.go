0bce26a9c1dd86ccb33f6ac719ac06c748cb4f29
viliusan <vilius.andraitis@gmail.com>
2023-12-02 01:05:17 +0200

    add completed day1 part2

--- day1/main.go
+++ day1/main.go
@@ -69,17 +69,86 @@ func part1(input string) int {
 }
 
 func part2(input string) int {
-	return 0
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	regex := regexp.MustCompile(`[0-9]|eightwo|oneight|twone|one|two|three|four|five|six|seven|eight|eight|nine`)
+	sum := 0
+
+	for scanner.Scan() {
+        line := scanner.Text()
+		readLine := regex.FindAllString(line, -1)
+		stringDigits := parseLine(readLine)
+		parsedDigits := parseDigits(stringDigits)
+		firstDigit := parsedDigits[0]
+		lastDigit := parsedDigits[len(parsedDigits)-1]
+		sum += firstDigit * 10 + lastDigit
+    }
+
+	return sum
 }
 
-func parseInput(input string) (parsedInput []int) {
-	for _, line := range strings.Split(input, "\n") {
-		parsedInput = append(parsedInput, stringToInt(line))
+func parseDigits(parsedDigits []string) []int {
+	var newArray []int
+
+	for i := 0; i <= len(parsedDigits)-1; i++ {
+		var firstDigit int
+		var secondDigit int
+
+		parsedInt := stringToInt(parsedDigits[i])
+		if parsedInt > 9 {
+			firstDigit = parsedInt / 10
+			secondDigit = parsedInt % 10
+
+			newArray = append(newArray, firstDigit, secondDigit)
+		} else {
+			newArray = append(newArray, parsedInt)
+		}
 	}
-	return parsedInput
+
+	return newArray
 }
 
 func stringToInt(input string) int {
 	output, _ := strconv.Atoi(input)
 	return output
+}
+
+func parseLine(input []string) []string {
+	for i := 0; i <= len(input)-1; i++ {
+		_, error := strconv.Atoi(input[i])
+		if error != nil {
+			input[i] = wordToDigit(input[i])
+			}
+		}
+	return input
+}
+
+func wordToDigit(input string) string  {
+	switch input {
+	case "one":
+		return "1"
+	case "two":
+		return "2"
+	case "three":
+		return "3"
+	case "four":
+		return "4"
+	case "five":
+		return "5"
+	case "six":
+		return "6"
+	case "seven":
+		return "7"
+	case "eight":
+		return "8"
+	case "nine":
+		return "9"
+	case "eightwo":
+		return "82"
+	case "oneight":
+		return "18"
+	case "twone":
+		return "21"
+	default:
+		panic("An error occured while parsing")
+	}
 }
